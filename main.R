e5f2415c6ebcb7bf095ef5e95829d51b5c4f7334
mindy Tran <mindyt5@scc-wi1.scc.bu.edu>
2024-10-16 01:32:41 -0400

    plot functions in main.R

--- main.R
+++ main.R
@@ -41,7 +41,7 @@ filter_zero_var_genes <- function(verse_counts) {
     mutate(row = row_number()) %>%
     pivot_longer(-row) %>%
     group_by(row) %>%
-    summarize(var = var(value)) %>%
+    summarize(var = var(value)) %>% # variance calculation
     bind_cols(verse_counts, .) %>% 
     # filter out zero variance rows
     filter(var!=0) %>% 
@@ -62,7 +62,7 @@ filter_zero_var_genes <- function(verse_counts) {
 #' output:`"Ad"`
 
 timepoint_from_sample <- function(x) {
-  result <- substring(x, 2,3)
+  result <- substring(x, 2,3) # take just the timepoint part
   return(result)
 }
 
@@ -77,10 +77,10 @@ timepoint_from_sample <- function(x) {
 #' output: `"1"`
 
 sample_replicate <- function(x) {
-  result <- str_split(x, '_')[[1]][2] # split by _ and get second value
+  result <- str_split(x, '_', simplify = TRUE)[, 2] # split by _ and get second value for the replicate
   return(result)
 }
-# sample_replicate("vAd_1")
+
 
 
 #' Generate sample-level metadata from sample names.
@@ -110,6 +110,9 @@ meta_info_from_labels <- function(sample_names) {
 }
 
 
+
+
+
 #' Calculate total read counts for each sample in a count data.
 #'
 #'
@@ -122,20 +125,13 @@ meta_info_from_labels <- function(sample_names) {
 #' @examples `get_library_size(count_data)`
 
 get_library_size <- function(count_data) {
-  result <- verse_counts %>% select(-gene) %>% 
-    summarise(across(everything(), sum)) %>%
-    pivot_longer(cols = everything(), names_to = "sample", values_to = "value")
-  
+  result <- count_data %>% select(-gene) %>% 
+    summarise(across(everything(), sum)) %>% # sum the columns
+    pivot_longer(cols = everything(), names_to = "sample", values_to = "value") # pivot to desired form
   return(result)
 }
 
 
-# helper function for normalize_by_cpm
-normalize <- function(x, sample_library_size){
-  # creating function for normalizing values
-  # count / (sample_library_size/10^6)
-  return(x / (sample_library_size/10**6))
-}
 
 #' Normalize raw count data to counts per million WITH pseudocounts using the
 #' following formula:
@@ -150,15 +146,21 @@ normalize <- function(x, sample_library_size){
 #' @examples
 #' `normalize_by_cpm(count_data)`
 normalize_by_cpm <- function(count_data) {
-  #Calculate the number of reads in each sample
+  # number of reads in each sample
   reads_cts <- get_library_size(count_data)
-  sample_library_size <- sum(reads_cts$value)
   df <- as.data.frame(count_data)
-  result <- lapply(df,normalize, sample_library_size)
-  return(result)
+  
+  # go through samples and library sizes
+  for (i in 1:length(reads_cts$sample)) {
+    sample_name <- reads_cts$sample[i]
+    library_size <- reads_cts$value[i]
+    
+    # count / (library size) * 10^6
+    df[[sample_name]] <- df[[sample_name]] / library_size * 10**6
+  }
+  
+  return(as_tibble(df))
 }
-
-normalize_by_cpm(verse_counts)
 
 
 #' Normalize raw count matrix using DESeq2
@@ -175,8 +177,21 @@ normalize_by_cpm(verse_counts)
 #' @examples
 #' `deseq_normalize(count_data, meta_data)`
 deseq_normalize <- function(count_data, meta_data) {
-    return(NULL)
+  mat <- count_data %>% select(-gene) %>% as.matrix() # matrix for countData
+  rownames(mat) <- count_data %>%  pull(gene)
+  # making a summarized experiment object
+  dds <- DESeqDataSetFromMatrix(countData = mat,
+                                colData = meta_data,
+                                design = ~ 1) 
+  dds <- DESeq(dds)
+  # normalize
+  normalized_counts <- counts(dds, normalized=TRUE) %>% 
+    as.data.frame %>% 
+    rownames_to_column(var = 'gene') %>% 
+    as_tibble()
+  return(normalized_counts)
 }
+
 
 
 #' Perform and plot PCA using processed data.
@@ -195,7 +210,30 @@ deseq_normalize <- function(count_data, meta_data) {
 #' `plot_pca(data, meta, "Raw Count PCA")`
 
 plot_pca <- function(data, meta, title="") {
-    return(NULL)
+  # use plotPCA from DESeq2
+  # set up dds
+  mat <- data %>% select(-gene) %>% as.matrix() # matrix for countData
+  rownames(mat) <- data %>%  pull(gene)
+  # making a summarized experiment object
+  dds <- DESeqDataSetFromMatrix(countData = round(mat), # DESeqDataSet needs countData to be non-negative integers
+                                colData = meta,
+                                design = ~ timepoint) 
+  dds <- DESeq(dds)
+  
+  # DESeq2 vignette and use either the VST or RLog transformation on the raw counts
+  rld <- rlog(dds, blind=FALSE)
+  
+  # PCA and plot
+  pcas <- plotPCA(rld, intgroup = "timepoint", returnData = TRUE)
+  percent_var <- round(100 * attr(pcas, "percentVar"))  # Get variance explained by each PC
+
+  # Step 4: Create PCA plot using ggplot2
+  g <- ggplot(pcas, aes(x = PC1, y = PC2, color = timepoint)) +
+    geom_point() +
+    labs(title = title,
+         x = paste0("PC1: ", percent_var[1], "% variance"),
+         y = paste0("PC2: ", percent_var[2], "% variance"))
+  return(g)
 }
 
 
@@ -212,7 +250,17 @@ plot_pca <- function(data, meta, title="") {
 #' @example `plot_sample_distributions(data, scale_y_axis=TRUE, title='Raw Count Distributions')`
 
 plot_sample_distributions <- function(data, scale_y_axis=FALSE, title="") {
-    return(NULL)
+  data <- data %>%
+    pivot_longer(cols = -gene, names_to = "sample", values_to = "counts")
+  g <- ggplot(data, aes(x=fct_inorder(sample), y=counts, color=sample)) +
+    geom_boxplot() + 
+    labs(title = title,
+         x ="sample", y = "counts")
+  
+  if (scale_y_axis==TRUE){
+    g <- g + scale_y_continuous(trans='log10')
+  }
+  return(g)
 }
 
 
@@ -233,6 +281,23 @@ plot_sample_distributions <- function(data, scale_y_axis=FALSE, title="") {
 #' @example `plot_variance_vs_mean(data, scale_y_axis=TRUE, title='variance vs mean (raw counts)')`
 
 plot_variance_vs_mean <- function(data, scale_y_axis=FALSE, title="") {
-    return(NULL)
+  data <- data %>% rowwise() %>%
+    mutate(mean_value = mean(c_across(-gene)), # mean for genes
+           variance = var(c_across(-gene))) %>% # var for genes
+    ungroup() %>% 
+    arrange(mean_value) %>% # order by rank mean count
+    mutate(rank = row_number()) # set ranks
+  
+  # scatter plot
+  g <- ggplot(data, aes(x = rank, y = variance)) +
+    geom_point(alpha = 0.5) + # transparency increased 
+    geom_smooth(aes(y = variance, x = rank), span = .2, se = FALSE, color = "blue") + # smooth line
+    labs(title = title,
+         x ="Rank(Mean)", y = "Variance")
+  
+  if (scale_y_axis==TRUE){
+    g <- g + scale_y_continuous(trans='log10')
+  }
+  return(g)
 }
 
